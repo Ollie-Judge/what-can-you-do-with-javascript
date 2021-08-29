3470c6bd7ee01635a1a48205c92e19898d51712a
Ollie-Judge <ollie.judge@hotmail.com>
2021-08-29 16:59:54 +0000

    added an event listener that when clicked creates a p element that is filled with inof from the input box, also added a cross off function and a remove function

---
+++ assets/script/todo.js
@@ -0,0 +1,17 @@
+let addItem = document.getElementById('addItem');
+let todoList = document.getElementById('todoList');
+let inputBox = document.getElementById('inputBox');
+
+addItem.addEventListener ('click', function(){
+    var info = docuemnt.createElement ('p')
+    info.innerText = inputBox.value;
+    todoList.appendChild(info);
+    inputBox.value = "";
+
+    info.addEventListener('click', function() {
+        info.style.textDecoration = "line-through";
+    })
+    info.addEventListener('dblClick', function() {
+        todoList.removeChild(info);
+    })
+})
