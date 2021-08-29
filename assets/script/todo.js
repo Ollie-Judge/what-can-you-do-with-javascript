e829eef4b36bca59473a1c4eb2843a20ad856472
Ollie-Judge <ollie.judge@hotmail.com>
2021-08-29 18:12:55 +0000

    made spelling corrections

--- assets/script/todo.js
+++ assets/script/todo.js
@@ -2,16 +2,16 @@ let addItem = document.getElementById('addItem');
 let todoList = document.getElementById('todoList');
 let inputBox = document.getElementById('inputBox');
 
-addItem.addEventListener ('click', function(){
-    var info = docuemnt.createElement ('p')
+addItem.addEventListener('click', function() {
+    var info = document.createElement('p')
     info.innerText = inputBox.value;
     todoList.appendChild(info);
-    inputBox.value = "";
+    inputBox.value ="";
 
     info.addEventListener('click', function() {
         info.style.textDecoration = "line-through";
     })
-    info.addEventListener('dblClick', function() {
+    info.addEventListener('dblclick', function() {
         todoList.removeChild(info);
     })
 })
